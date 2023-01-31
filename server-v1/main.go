0f32f1fc2237a2ebfd9f3f199acdb6a645891e2f
EKky Kharismadhany <ekky.kharismadhany@bluebirdgroup.com>
2023-01-31 14:24:36 +0700

    add server v-1

---
+++ server-v1/main.go
@@ -0,0 +1,33 @@
+package main
+
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	payload := struct {
+		Payload string `json:"payload"`
+	}{
+		Payload: "halo bang",
+	}
+	byteData, err := json.Marshal(payload)
+	if err != nil {
+		log.Println(err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(byteData)
+}
+
+func main() {
+	log.Println("server v1 starting")
+	http.HandleFunc("/hello", HelloHandler)
+	if err := http.ListenAndServe("0.0.0.0:9090", nil); err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+}
